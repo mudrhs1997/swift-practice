b7308ddbd6cd101bf0fc3fa5860013409fbe8456
mudrhs1997 <mudrhs1997@daum.net>
2022-03-16 21:57:56 +0900

    chatapp 1

--- ChartMultiplerApp/ChartMultiplerApp/ViewController.swift
+++ ChartMultiplerApp/ChartMultiplerApp/ViewController.swift
@@ -8,9 +8,11 @@
 import UIKit
 
 
+//  NSLayoutConstraint 자체를 새로 만들어서 적용한다. multiplier 이외의 값은 그냥 가져온다.
 extension NSLayoutConstraint {
     func changeMultiplier(value: CGFloat) -> NSLayoutConstraint{
         
+        //  본래의 값을 비활성화 시킨다 ( 없으면 constraint 누적 )
         NSLayoutConstraint.deactivate([self])
         
         let newLayoutConstraint = NSLayoutConstraint.init(item: self.firstItem, attribute: self.firstAttribute, relatedBy: self.relation, toItem: self.secondItem, attribute: self.secondAttribute, multiplier: value, constant: self.constant)
@@ -19,6 +21,7 @@ extension NSLayoutConstraint {
         newLayoutConstraint.shouldBeArchived = self.shouldBeArchived
         newLayoutConstraint.identifier = self.identifier
         
+        //  새로 만든 것으로 활성화
         NSLayoutConstraint.activate([newLayoutConstraint])
         
         return newLayoutConstraint
@@ -32,6 +35,7 @@ class ViewController: UIViewController {
     @IBOutlet weak var graph3Height: NSLayoutConstraint!
     @IBOutlet weak var graph4Height: NSLayoutConstraint!
     @IBOutlet weak var graph5Height: NSLayoutConstraint!
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
---
+++ ChatApp/ChatApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ChatApp
+//
+//  Created by 정명곤 on 2022/03/16.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
