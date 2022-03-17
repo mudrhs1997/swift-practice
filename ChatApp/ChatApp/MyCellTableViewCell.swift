15b7c1abe0466773e6c5554792510b768180a3b0
mudrhs1997 <mudrhs1997@daum.net>
2022-03-17 22:28:30 +0900

    chatapp2

---
+++ ChatApp/ChatApp/MyCellTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  MyCellTableViewCell.swift
+//  ChatApp
+//
+//  Created by 정명곤 on 2022/03/17.
+//
+
+import UIKit
+
+class MyCellTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
